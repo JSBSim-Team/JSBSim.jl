693b7b9d66ce87bbebb3cc05aeadbd883254123b
Bertrand Coconnier <bcoconni@users.sf.net>
2022-10-15 15:37:48 +0200

    Use artifacts for FDM data rather than cloning JSBSim.

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,25 +1,21 @@
 using Test
 using JSBSim
 
-@test isdir(JSBSim.GetDefaultRootDir()) == true
 path = JSBSim.SGPath("Hello world!")
 @test JSBSim.str(path) == "Hello world!"
 
-root_dir = joinpath(@__DIR__, "..", "JSBSim")
+root_dir = JSBSim.GetDefaultRootDir()
+@test isdir(root_dir) == true
 
 fdm = JSBSim.FGFDMExec()
 JSBSim.SetRootDir(fdm, root_dir)
 
 @test JSBSim.GetRootDir(fdm) == root_dir
 
-if isdir(root_dir)
-  @test JSBSim.LoadModel(fdm, "737") == true
-  @test JSBSim.LoadIC(fdm, "cruise_init.xml", true) == true
-  @test JSBSim.RunIC(fdm) == true
+@test JSBSim.LoadModel(fdm, "737") == true
+@test JSBSim.LoadIC(fdm, "cruise_init.xml", true) == true
+@test JSBSim.RunIC(fdm) == true
 
-  while JSBSim.GetPropertyValue(fdm, "simulation/sim-time-sec") < 5.0
-    JSBSim.Run(fdm)
-  end
-else
-  println("JSBSim directory NOT FOUND. Skip...")
+while JSBSim.GetPropertyValue(fdm, "simulation/sim-time-sec") < 5.0
+  JSBSim.Run(fdm)
 end
